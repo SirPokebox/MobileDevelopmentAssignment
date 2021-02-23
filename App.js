2d503d45e7ccf388d7adf24c1ace9de474293ab7
SirPokebox <rhysclinch@gmail.com>
2021-02-23 17:40:12 +0000

    Removed coffida server from components. Should not be there. Linted App.js

--- App.js
+++ App.js
@@ -1,11 +1,8 @@
-import 'react-native-gesture-handler';
+import 'react-native-gesture-handler'
 
-
-import React, { Component } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-
-
+import React, { Component } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
 
 import Home from './components/Login.js'
 import Register from './components/SignUp.js'
@@ -19,29 +16,28 @@ import UpdateReviews from './components/UpdateReview.js'
 import ViewAllReviews from './components/AllReviews.js'
 import FavouritePlace from './components/SelectFavourite.js'
 
-const Stack = createStackNavigator();
+const Stack = createStackNavigator()
 
-class App extends Component{
-
-  render(){
-    return(
+class App extends Component {
+  render () {
+    return (
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen options={{headerShown: false}} name="Home" component ={Home}/>
-          <Stack.Screen name="Register" component ={Register}/>
-          <Stack.Screen options= {{headerShown: false}} name="Coffee" component ={Coffee}/>
-          <Stack.Screen name="Map View" component ={UserLocation}/>
-          <Stack.Screen name="MakeReview" component ={MakeReview}/>
-          <Stack.Screen options= {{headerShown: false}} name="UserProfile" component ={UserProfile}/>
-          <Stack.Screen options= {{headerShown: false}} name="UserReviews" component ={UserReviews}/>
-          <Stack.Screen options = {{headerShown: false}} name="FavouriteCoffee" component ={FavouriteCoffee}/>
-          <Stack.Screen options = {{headerShown: false}} name="ViewAllReviews" component ={ViewAllReviews}/>
-          <Stack.Screen name="UpdateReviews" component ={UpdateReviews}/>
-          <Stack.Screen options = {{headerShown: false}} name="FavouritePlace" component ={FavouritePlace}/>
+          <Stack.Screen options={{ headerShown: false }} name='Home' component={Home} />
+          <Stack.Screen name='Register' component={Register} />
+          <Stack.Screen options={{ headerShown: false }} name='Coffee' component={Coffee} />
+          <Stack.Screen name='Map View' component={UserLocation} />
+          <Stack.Screen name='MakeReview' component={MakeReview} />
+          <Stack.Screen options={{ headerShown: false }} name='UserProfile' component={UserProfile} />
+          <Stack.Screen options={{ headerShown: false }} name='UserReviews' component={UserReviews} />
+          <Stack.Screen options={{ headerShown: false }} name='FavouriteCoffee' component={FavouriteCoffee} />
+          <Stack.Screen options={{ headerShown: false }} name='ViewAllReviews' component={ViewAllReviews} />
+          <Stack.Screen name='UpdateReviews' component={UpdateReviews} />
+          <Stack.Screen options={{ headerShown: false }} name='FavouritePlace' component={FavouritePlace} />
 
         </Stack.Navigator>
       </NavigationContainer>
-      );
+    )
   }
 }
-export default App;
+export default App
